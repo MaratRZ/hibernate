26c7ee8b3a576c7482456fcf6a96baeda33db938
Marat Zalyaletdinov <marat.rz@gmail.com>
2021-12-26 22:38:54 +0300

    homework05 - Java Persistence API. Hibernate. Part 1

--- src/main/java/ru/gb/dao/ProductDao.java
+++ src/main/java/ru/gb/dao/ProductDao.java
@@ -1,4 +1,12 @@
 package ru.gb.dao;
 
+import ru.gb.entity.Product;
+
 public interface ProductDao {
+    Iterable<Product> findAll();
+    Product findById(Long id);
+    String findTitleById(Long id);
+    void insert(Product product);
+    void update(Product product);
+    void deleteById(Long id);
 }
